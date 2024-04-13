efb0a08eb59b421ceaf0a935c33f75e9fddda16d
ContardoRM <contardo.acuna@gmail.com>
2024-04-12 21:16:37 -0400

    testing fix

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 
 from master import Master
